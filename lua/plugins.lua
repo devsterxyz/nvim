7ae05a522da21ecf9ad3e5be5862aefecff9b9b3
Dev <sharmadevdeep9@gmail.com>
2026-06-26 10:50:38 +0530

    Mason

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,76 +12,99 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme kanagawa")
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    build = ":TSUpdate",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
+  spec = {
+    {
+      "rebelot/kanagawa.nvim",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.cmd("colorscheme kanagawa")
+      end,
     },
-    config = function()
-      require("nvim-treesitter.configs").setup({  -- removed the stray require() call
-        ensure_installed = {
-          "c",
-          "cpp",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "javascript",
-          "typescript",
-        },
-        auto_install = true,
-        highlight = {
-          enable = true,
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<leader>ss",
-            node_incremental = "<leader>si",
-            scope_incremental = "<leader>sc",
-            node_decremental = "<leader>sd",
+    {
+      "nvim-treesitter/nvim-treesitter",
+      branch = "master",
+      build = ":TSUpdate",
+      dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+      },
+      config = function()
+        require("nvim-treesitter.configs").setup({
+          ensure_installed = {
+            "c",
+            "cpp",
+            "lua",
+            "vim",
+            "vimdoc",
+            "query",
+            "javascript",
+            "typescript",
           },
-        },
-        textobjects = {
-          select = {
+          auto_install = true,
+          highlight = {
             enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = { query = "@class.inner", desc = "Select inner part of class region" },
-              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-            },
-            selection_modes = {
-              ["@parameter.outer"] = "v",
-              ["@function.outer"] = "V",
-              ["@class.outer"] = "<c-v>",
-            },
-            include_surrounding_whitespace = true,
           },
-        },
-      })
-    end,
+          incremental_selection = {
+            enable = true,
+            keymaps = {
+              init_selection = "<leader>ss",
+              node_incremental = "<leader>si",
+              scope_incremental = "<leader>sc",
+              node_decremental = "<leader>sd",
+            },
+          },
+          textobjects = {
+            select = {
+              enable = true,
+              lookahead = true,
+              keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = { query = "@class.inner", desc = "Select inner part of class region" },
+                ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+              },
+              selection_modes = {
+                ["@parameter.outer"] = "v",
+                ["@function.outer"] = "V",
+                ["@class.outer"] = "<c-v>",
+              },
+              include_surrounding_whitespace = true,
+            },
+          },
+        })
+      end,
+    },
+    {
+      "mason-org/mason.nvim",
+      config = function()
+        require("mason").setup()
+      end,
+    },
+    {
+      "mason-org/mason-lspconfig.nvim",
+      dependencies = {
+        "mason-org/mason.nvim",
+        "neovim/nvim-lspconfig",
+      },
+      config = function()
+        require("mason-lspconfig").setup({
+          ensure_installed = { "clangd", "lua_ls", "biome" },
+          -- automatic_enable = true is the default; servers installed
+          -- via Mason get vim.lsp.enable()'d for you automatically.
+        })
+
+        -- Optional: server-specific settings still go through lspconfig/vim.lsp.config
+        vim.lsp.config("lua_ls", {
+          settings = {
+            Lua = {
+              diagnostics = { globals = { "vim" } },
+            },
+          },
+        })
+      end,
+    },
   },
-  {                               -- moved outside the opts table, now a proper plugin entry
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.clangd.setup({})
-    end,
-  },
-}, {
   performance = {
     rtp = {
       reset = false,
